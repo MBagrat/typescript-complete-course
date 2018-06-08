4baa0e06ab0b77b05b4c3fb6de34ab8b25aa3588
Bagrat Mukaelyan <bagrat.mukaelyan@gmail.com>
2018-06-08 09:32:16 +0400

    added module one sources

---
+++ module_2/script.ts
@@ -0,0 +1,59 @@
+// string
+let myName: string = 'Bagrat';
+// myName = 5;
+
+// number
+let myAge: number = 29;
+// myAge = 'Bagrat';
+
+// boolean
+let hasHobbies: boolean = false;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27';
+
+//array
+let hobbies: any[] = [ "Cooking", "Sports" ];
+hobbies = [ 100 ];
+// hobbies = 100;
+
+// tuples
+let address: [ string, number ] = [ "Superstreet", 99 ];
+
+// enum
+enum Color {
+  Gray, //0
+  Green = 100, //100
+  Blue = 2 //2
+}
+
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = {brand: "BMW", series: 3};
+console.log(car);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
