41380efa76e11cbd135354552837933ab51fc8d3
Bagrat Mukaelyan <bagrat.mukaelyan@gmail.com>
2018-06-09 14:51:53 +0400

    added module two sources.

--- module_2/script.js
+++ module_2/script.js
@@ -32,3 +32,79 @@ var car = "BMW";
 console.log(car);
 car = { brand: "BMW", series: 3 };
 console.log(car);
+
+// functions
+function returnMyName() {
+    return myName;
+}
+
+console.log(returnMyName());
+
+// void
+function sayHello() {
+    console.log("Hello!");
+}
+
+sayHello();
+
+// arguments type
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+
+// console.log(multiply(5, "Bagrat"));
+console.log(multiply(5, 20));
+// function types
+var myMultiply;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5, 5));
+// objects
+var userData = {
+    name: "Bagrat",
+    age: 28
+};
+// userData = {
+//   a: "Hello",
+//   b: 22
+// };
+// complex object
+var complexx = {
+    data: [10, 20, 30, 40, 50],
+    output: function (all) {
+        return this.data;
+    }
+};
+var complex2 = {
+    data: [10, 20, 30, 40, 50],
+    output: function (all) {
+        return this.data;
+    }
+};
+// union types
+var myRealRealAge = 27;
+myRealRealAge = "27";
+// myRealRealAge = true;
+// if (typeof myRealRealAge == "number | string"){
+//   console.log("Union type don't working");
+// }
+// check types
+var finalValue = "Some Text";
+if (typeof finalValue == "string") {
+    console.log("Final value is a string");
+}
+
+// never
+function neverReturn() {
+    throw new Error();
+}
+
+// Nullable Types
+var canBeNull = 12;
+canBeNull = null;
+// Now type is undefined
+var canAlsoBeNull;
+canAlsoBeNull = null;
+var canThisBeNull;
+canThisBeNull = 12;
--- module_2/script.ts
+++ module_2/script.ts
@@ -36,24 +36,92 @@ console.log(myColor);
 // any
 let car: any = "BMW";
 console.log(car);
-car = {brand: "BMW", series: 3};
+car = { brand: "BMW", series: 3 };
 console.log(car);
 
+// functions
+function returnMyName(): string {
+  return myName;
+}
 
+console.log(returnMyName());
 
+// void
+function sayHello(): void {
+  console.log("Hello!");
+}
 
+sayHello();
 
+// arguments type
+function multiply(value1: number, value2: number): number {
+  return value1 * value2;
+}
 
+// console.log(multiply(5, "Bagrat"));
+console.log(multiply(5, 20));
 
+// function types
+let myMultiply: (val1: number, val2: number) => number;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5, 5));
 
+// objects
+let userData: { name: string, age: number } = {
+  name: "Bagrat",
+  age: 28
+};
+// userData = {
+//   a: "Hello",
+//   b: 22
+// };
 
+// complex object
+let complexx: { data: number[], output: (all: boolean) => number[] } = {
+  data: [ 10, 20, 30, 40, 50 ],
+  output: function (all: boolean): number[] {
+    return this.data;
+  }
+};
+// complex = {};
 
+// type alias
+type Complex = { data: number[], output: (all: boolean) => number[] };
 
+let complex2: Complex = {
+  data: [ 10, 20, 30, 40, 50 ],
+  output: function (all: boolean): number[] {
+    return this.data;
+  }
+};
 
+// union types
+let myRealRealAge: number | string = 27;
+myRealRealAge = "27";
+// myRealRealAge = true;
 
+// if (typeof myRealRealAge == "number | string"){
+//   console.log("Union type don't working");
+// }
 
+// check types
+let finalValue = "Some Text";
+if (typeof finalValue == "string") {
+  console.log("Final value is a string");
+}
 
+// never
+function neverReturn(): never {
+  throw new Error();
+}
 
-
-
-
+// Nullable Types
+let canBeNull: number | null = 12;
+canBeNull = null;
+// Now type is undefined
+let canAlsoBeNull;
+canAlsoBeNull = null;
+let canThisBeNull: number | null;
+canThisBeNull = 12;
