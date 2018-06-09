7899c0892fac1fcf91f66d39d4e6bede89bf085f
Bagrat Mukaelyan <bagrat.mukaelyan@gmail.com>
2018-06-09 19:49:21 +0400

    added module three sources

---
+++ module_3/script.js
@@ -0,0 +1,13 @@
+var yourName = "Bagrat";
+var yourAge = 27;
+var anything;
+anything = 12;
+// myName = 30;
+function controleMe(isTrue, somethingElse) {
+    var result;
+    if (isTrue) {
+        result = 12;
+    }
+    result = 33;
+    return result;
+}
---
+++ module_3/script.ts
@@ -0,0 +1,15 @@
+let yourName: string = "Bagrat";
+let yourAge: number = 27;
+let anything;
+anything = 12;
+
+// myName = 30;
+
+function controleMe(isTrue: boolean, somethingElse: boolean) {
+  let result: number;
+  if (isTrue) {
+    result = 12;
+  }
+  result = 33;
+  return result;
+}
