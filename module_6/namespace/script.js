7f092a5221ab164ad226be66286d3bae410f31aa
Bagrat Mukaelyan <bagrat.mukaelyan@gmail.com>
2018-06-24 18:47:28 +0400

    removed compiled js files

--- module_6/namespace/script.js
+++
@@ -1,7 +0,0 @@
-/// <reference path="circleMath.ts" />
-/// <reference path="rectangleMath.ts" />
-var Circle = MyMath.Circle;
-var PI = 2.99;
-console.log(MyMath.calculateRectangle(10, 20));
-console.log(Circle.calculateCircumference(3));
-console.log(PI);
